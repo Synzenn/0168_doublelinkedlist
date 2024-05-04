cf749f09071025cf31204bdd4725d0e71b71cbc7
Synzenn <velriel42@gmail.com>
2024-05-04 16:09:25 +0700

    mulai membuat bool search

--- Algoritmaaaaaaa/Algoritmaaaaaaa.cpp
+++ Algoritmaaaaaaa/Algoritmaaaaaaa.cpp
@@ -31,7 +31,31 @@ void addNode(){
 		START = newNode;
 	}
 	else {
+		Node* current = START;
+		Node* previous = NULL;
 
+		while (current != NULL && current->noMhs < newNode->noMhs) {
+			previous = current;
+			current = current->next;
+		}
+
+		newNode->next = current;
+		newNode->prev = previous;
+
+		if (current != NULL) {
+			current->prev = newNode;
+		}
+		if (current != NULL) {
+			current->next = newNode;
+		}
+		else{
+			START = newNode;
+		}
 	}
+}
+
+bool search(int rollNo, Node** previous, Node** current)
+{
+    
 }
 
