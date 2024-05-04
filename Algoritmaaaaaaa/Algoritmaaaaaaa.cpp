dfa3480cc6482e910226bcc21a4820af28606add
Synzenn <velriel42@gmail.com>
2024-05-04 16:14:46 +0700

    menyelesaikan void traverse

--- Algoritmaaaaaaa/Algoritmaaaaaaa.cpp
+++ Algoritmaaaaaaa/Algoritmaaaaaaa.cpp
@@ -126,7 +126,18 @@ bool listempty()
 
 void traverse()
 {
-   
+    if (listempty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecord is ascending order of roll number are:  " << endl;
+        Node* currentNode = START;              //step 1
+        while (currentNode != NULL)             //step 2
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;         //step 3
+            currentNode = currentNode->next;                                        //step 4
+        }
+    }
 }
 
 
