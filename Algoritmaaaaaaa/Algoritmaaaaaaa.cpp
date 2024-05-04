b02e099dc1f23280eb607047bff89d1b218d4e6e
Synzenn <velriel42@gmail.com>
2024-05-04 16:15:39 +0700

    menyelesaikan void revtraverse

--- Algoritmaaaaaaa/Algoritmaaaaaaa.cpp
+++ Algoritmaaaaaaa/Algoritmaaaaaaa.cpp
@@ -142,7 +142,21 @@ void traverse()
 
 void revtraverse()
 {
-    
+    if (listempty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecord is descending order of roll number are:  " << endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
 }
 
 
